8806a5e2a62910a990f92794555fddf95babd5c5
rafkaputra221 <rafkaputra2007@gmail.com>
2024-10-22 12:11:32 +0700

    versi 0.3

--- main.dart
+++ main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 54, 37, 103)),
         ),
         home: MyHomePage(),
       ),
@@ -39,27 +39,62 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+     var pair = appState.current;  // variabel pair menyimpan kata yang sedang tampil/aktif
+
 
    // ...
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-
-          // ↓ Add this.
-          ElevatedButton( // membuat button di dalam body
-            onPressed: () { // fungsi yang dieksekusi ketika button di tekan
-             appState.getNext();
-          
-            },
-            child: Text('Next'),
-          ),
-
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random AWESOME idea:'),
+            BigCard(pair: pair), // mengambil nilai dari variabel  pair , lalu diubah menjadi huruf kecil , ditampilkan sebagai bigcard 
+        
+            // ↓ Add this.
+            ElevatedButton( // membuat button di dalam body
+              onPressed: () { // fungsi yang dieksekusi ketika button di tekan
+               appState.getNext();
+            
+              },
+              child: Text('Next'),
+            ),
+        
+          ],
+        ),
       ),
     );
 
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+   
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);       // ← Add this.
+     // ↓ Add this.
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+ return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        // ↓ Change this line.
+        child: Text(pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",),
+      ),
+    );
   }
 }
