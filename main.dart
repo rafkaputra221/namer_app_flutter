421a6c24869645adb8600661aba6af7f31faeaaf
rafkaputra221 <rafkaputra2007@gmail.com>
2024-10-30 14:18:05 +0700

    versi 0.4

--- main.dart
+++ main.dart
@@ -27,47 +27,142 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-   // ↓ Add this.
+ 
   void getNext() {
-    current = WordPair.random();
-    notifyListeners();
+    current = WordPair.random(); // acak kata 
+    notifyListeners(); // kirim kata yg  diacak ke lisanter 
+  }
+  // membuat variabel bertipe "list"/daftar  bernama favorites untuk menyimpan daftar kata yang di like
+  var favorites = <WordPair>[];
+  // fungsi untuk menanbahkan kata ke dalam , atua menghapus kata dari list favorit
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current); // menghapus kata dari list favorit
+    } else {
+      favorites.add(current); // menambah kata ke list favorit 
+    }
+    notifyListeners(); // menempelkan fungsi ini ke button like supaya button like bisa mengetahui jika  dirinya sedang ditekan
   }
 }
+
+
 // ...
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget  {
+  
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// ...
+
+// ...
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: false,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,  // ← Here.
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+// ...
+// ...
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-     var pair = appState.current;  // variabel pair menyimpan kata yang sedang tampil/aktif
+    var pair = appState.current;
 
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
-   // ...
-
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('A random AWESOME idea:'),
-            BigCard(pair: pair), // mengambil nilai dari variabel  pair , lalu diubah menjadi huruf kecil , ditampilkan sebagai bigcard 
-        
-            // ↓ Add this.
-            ElevatedButton( // membuat button di dalam body
-              onPressed: () { // fungsi yang dieksekusi ketika button di tekan
-               appState.getNext();
-            
-              },
-              child: Text('Next'),
-            ),
-        
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
-
   }
 }
+
+// ...
 
 class BigCard extends StatelessWidget {
   const BigCard({
